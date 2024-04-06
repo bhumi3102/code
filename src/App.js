6e2c984fbf860f9bf015b101e25f23c76497628f
muizAyomide <Akinrinolamuiz22@gmail.com>
2024-04-06 14:16:06 +0100

    App creation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css'
+import MainDash from './components/MainDash/MainDash';
+import Sidebar from './components/Sidebar/Sidebar';
+
+
+
 
 function App() {
   return (
     <div className="App">
       <div className="AppGlass">
-
+    <Sidebar/>
+    <MainDash/>
       </div>
     </div>
   );
