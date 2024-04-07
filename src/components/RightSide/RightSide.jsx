783f8bc6ff7b4f6d43bd876ac3cd92bc9122bd44
muizAyomide <Akinrinolamuiz22@gmail.com>
2024-04-07 13:18:44 +0100

    right side c reation

---
+++ src/components/RightSide/RightSide.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './RightSide.css'
+const RightSide = () => {
+  return (
+    <div className='right-side'>
+      <div>
+        <h3>Updates</h3>
+        <Updates/>
+      </div>
+      <div>
+        <h3>Customer Review</h3>
+      </div>
+    </div>
+  )
+}
+
+export default RightSide
