0ecaeecd9f5ebc71d8ad0fe69f799bf3efdb0b8e
muizAyomide <Akinrinolamuiz22@gmail.com>
2024-04-20 02:47:20 +0100

    commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import StoreCongtextProvider from "../src/components/context";
 
 ReactDOM.render(
-    <App />,
-  document.getElementById('root')
+  <StoreCongtextProvider>
+    <App />
+  </StoreCongtextProvider>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
